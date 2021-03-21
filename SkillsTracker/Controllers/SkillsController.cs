48130a7c1cc13b2e1c69b16ed6cf459032a830ac
lcc <sub01@iaonline.net>
2021-03-20 20:27:10 -0500

    Add project files.

---
+++ SkillsTracker/Controllers/SkillsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkillsTracker.Controllers
+{
+    public class SkillsController : Controller
+    {
+        public IActionResult Index()
+        {
+            string html = "<h1>Skills Tracker";
+            return Content(html, "text/html");
+            //return View();
+        }
+
+        public IActionResult Skills()
+        {
+            string html = "<h1>Skills Tracker";
+            return Content(html, "text/html");
+        }
+
+    } // class
+} // name
